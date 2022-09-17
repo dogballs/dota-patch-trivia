90dd296b20eaf7901b3ff6b32cfe5cce3b52fc96
Michael <mradionov.oss@gmail.com>
2022-09-18 00:33:59 +1000

    Some bad cards

--- lib/bad-cards.ts
+++ lib/bad-cards.ts
@@ -1,4 +1,8 @@
 // ids of bad cards
-const badCards: string[] = [];
+const badCards: string[] = [
+  '7.31_hero_Patch-Notes',
+  '7.32_hero_Patch-Notes',
+  '7.32_hero_Articles',
+];
 
 export default badCards;
